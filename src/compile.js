02cf3c282d15308e0f006dd141a7dd33ffaf5193
wolflo <wardnick@protonmail.com>
2020-05-16 21:13:22 +0400

    Add kill prefix

--- src/compile.js
+++ src/compile.js
@@ -3,11 +3,24 @@ const fs = require('fs');
 const parser = require('../huff/src/parser');
 const compiler = require('../huff/src/compiler');
 
-const OUT_FILE = '../out/sandy.bin'
 const modulesPath = path.posix.resolve(__dirname, './huff_modules');
+const OUT_PATH = '../out/';
+const SANDY_OUT = OUT_PATH + '../out/sandy.bin';
+const KILL_PREFIX_OUT = OUT_PATH + '../out/kill_prefix.bin';
 
-const { inputMap, macros, jumptables } = parser.parseFile('sandy.huff', modulesPath);
-const main = parser.processMacro('MAIN', 0, [], macros, inputMap, jumptables).data.bytecode;
+const sandy = assembleMain('sandy.huff', modulesPath);
+const kill_prefix = assembleMain('kill_prefix.huff', modulesPath);
 
-fs.writeFileSync(path.posix.resolve(__dirname, OUT_FILE), main);
-console.log(`written bytecode to ${OUT_FILE}`);
+writeBin(SANDY_OUT, sandy);
+writeBin(KILL_PREFIX_OUT, kill_prefix);
+
+console.log(`bytecode written to ${OUT_PATH}`);
+
+function assembleMain(filename, modPath) {
+  const { inputMap, macros, jumptables } = parser.parseFile(filename, modPath);
+  return parser.processMacro('MAIN', 0, [], macros, inputMap, jumptables).data.bytecode;
+}
+
+function writeBin(file, bytecode) {
+  fs.writeFileSync(path.posix.resolve(__dirname, file), bytecode);
+}
